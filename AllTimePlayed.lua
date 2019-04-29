5981437dbe73e66c78cf9d4e366dffcf4366353d
LAMBOLEY Lucas <lucaslamboley@outlook.com>
2019-04-29 18:14:44 +0200

    0.03
    Add tooltip when mouseover minimap button, et print in chat when click on minimap bouton

--- AllTimePlayed.lua
+++ AllTimePlayed.lua
@@ -1,10 +1,46 @@
 local addon = LibStub("AceAddon-3.0"):NewAddon("AllTimePlayed", "AceConsole-3.0", "AceEvent-3.0")
 local icon = LibStub("LibDBIcon-1.0")
+local lqt = LibStub("LibQTip-1.0")
 local ldb = LibStub("LibDataBroker-1.1"):NewDataObject("AllTimePlayed!", {
   type = "data source",
   text = "AllTimePlayed!",
   icon = "Interface\\Icons\\INV_Eng_Clockworkegg",
 })
+
+function addon:DrawTooltip(anchor)
+  RequestPlayed()
+  local totaltime = 0
+
+  if not self.tooltip then
+    self.tooltip = lqt:Acquire("AllTimePlayedTooltip", 2)
+  end
+
+  local tooltip = self.tooltip
+  tooltip:SetScale(0.90)
+  tooltip:Clear()
+  tooltip:SmartAnchorTo(anchor)
+
+  for player,time in pairs(AllTimePlayedDB) do
+    if (type(time) == 'number') then
+      line, column = tooltip:AddLine()
+      tooltip:SetCell(line, 1, player..": ", "LEFT", 1)
+      tooltip:SetCell(line, 2, secondsToDays(time), "RIGHT")
+      totaltime = totaltime + time
+    end
+  end
+
+  line, column = tooltip:AddLine()
+  tooltip:SetCell(line, 1, "Total: ", "LEFT", 1)
+  tooltip:SetCell(line, 2, secondsToDays(totaltime), "RIGHT")
+
+  tooltip:UpdateScrolling()
+
+  tooltip:Show()
+end
+
+function addon:CloseTooltips()
+  self.tooltip:Hide()
+end
 
 AllTimePlayedDB = AllTimePlayedDB or {}
 local playedAddon = true
@@ -18,7 +54,17 @@ ChatFrame_DisplayTimePlayed = function(...)
   return o(...)
 end
 
-function ldb:OnEnter(motion) ShowPlaytime() end
+function ldb:OnClick()
+  ShowPlaytime()
+end
+
+function ldb:OnEnter(motion)
+  addon:DrawTooltip(self)
+end
+
+function ldb:OnLeave()
+  addon:CloseTooltips()
+end
 
 function addon:OnInitialize()
   self.db = LibStub("AceDB-3.0"):New("AllTimePlayedDB", {
@@ -34,9 +80,13 @@ end
 
 function addon:TIME_PLAYED_MSG(name, total, currentLevel) AllTimePlayedDB[UnitName("player")] = total end
 
-function ShowPlaytime()
+function RequestPlayed()
   playedAddon = true
   RequestTimePlayed()
+end
+
+function ShowPlaytime()
+  RequestPlayed()
 
   local totaltime = 0
   for player,time in pairs(AllTimePlayedDB) do
