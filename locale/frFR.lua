26ff5bea0cac4588904b617a22c5490b90681bea
LAMBOLEY Lucas <lucaslamboley@outlook.com>
2019-05-01 17:16:57 +0200

    0.05

---
+++ locale/frFR.lua
@@ -0,0 +1,14 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("AllTimePlayed", "frFR")
+if not L then return end
+
+L["Description"] = "Description"
+L["What does AllTimePlayed ?"] = "Que fais AllTimePlayed ?"
+L["It show the played time for all characters when pointer is on the minimap button."] = "Il affiche le temps total joué pour tout les personnages quand la souris est sur le bouton de la minimap."
+L["Clean data"] = "Nettoyer les données"
+L["Erase all data saved"] = "Supprime toutes les données sauvegardées"
+L["Total:"] = "Total:"
+L["Total time played"] = "Temps de jeu total"
+L[" days, "] = " jours, "
+L[" hours, "] = " heures, "
+L[" minutes, "] = " minutes, "
+L[" seconds"] = " secondes"
