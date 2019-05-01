bfc3b571636251fa0b9665433b447bf63469707a
LAMBOLEY Lucas <lucaslamboley@outlook.com>
2019-05-01 17:17:14 +0200

    0.05

---
+++ locale/enUS.lua
@@ -0,0 +1,13 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("AllTimePlayed", "enUS", true)
+
+L["Description"] = true
+L["What does AllTimePlayed ?"] = true
+L["It show the played time for all characters when pointer is on the minimap button."] = true
+L["Clean data"] = true
+L["Erase all data saved"] = true
+L["Total:"] = true
+L["Total time played"] = true
+L[" days, "] = true
+L[" hours, "] = true
+L[" minutes, "] = true
+L[" seconds"] = true
