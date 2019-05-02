1a050242ae8ef2601546c65922e51f0f25804b32
LAMBOLEY Lucas <lucaslamboley@outlook.com>
2019-05-02 02:04:21 +0200

    0.06

--- locale/frFR.lua
+++ locale/frFR.lua
@@ -1,6 +1,14 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("AllTimePlayed", "frFR")
 if not L then return end
 
+L["Colors"] = "Couleurs"
+L["Current character"] = "Personnage actuel"
+L["Color of current character"] = "Couleur du personnage actuel"
+L["Others characters"] = "Autres personnages"
+L["Color of others characters"] = "Couleur des autres personnages"
+L["Total"] = "Total"
+L["Color of total"] = "Couleur du total"
+L["ERROR - Should be a hexadecimal code"] = "ERREUR - Doit être un code hexadécimale"
 L["Description"] = "Description"
 L["What does AllTimePlayed ?"] = "Que fais AllTimePlayed ?"
 L["It show the played time for all characters when pointer is on the minimap button."] = "Il affiche le temps total joué pour tout les personnages quand la souris est sur le bouton de la minimap."
