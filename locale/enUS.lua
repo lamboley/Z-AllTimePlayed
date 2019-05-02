2dd9da2dcad89ee23e4705dc552c6e1c0063365a
LAMBOLEY Lucas <lucaslamboley@outlook.com>
2019-05-02 20:29:04 +0200

    1.00

--- locale/enUS.lua
+++ locale/enUS.lua
@@ -1,20 +1,42 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("AllTimePlayed", "enUS", true)
 
+L["Clean current character"] = true
+L["Erase current character data"] = true
+L["Clean others character"] = true
+L["Erase others characters data"] = true
+
 L["Colors"] = true
+
+L["Color in minimap button"] = true
+L["Color in chat"] = true
+
 L["Current character"] = true
 L["Color of current character"] = true
 L["Others characters"] = true
 L["Color of others characters"] = true
 L["Total"] = true
 L["Color of total"] = true
-L["ERROR - Should be a hexadecimal code"] = true
-L["Description"] = true
-L["What does AllTimePlayed ?"] = true
-L["It show the played time for all characters when pointer is on the minimap button."] = true
-L["Clean data"] = true
-L["Erase all data saved"] = true
+
+L["FAQ"] = true
+L["Frequently Asked Questions"] = true
+L["What is AllTimePlayed ?"] = true
+L["It's an addon which record the played time per character. It give multiple way to print the info to the user."] = true
+L["When data are updated ?"] = true
+L["Data are updated when"] = true
+L["You log in."] = true
+L["You log out."] = true
+L["You change of zone."] = true
+L["You reload your UI."] = true
+L["You execute the /played command."] = true
+L["I have found a bug, how can I contact you ?"] = true
+L["You can create an issue on my github |cffffff78<https://github.com/lamboley/AllTimePlayed>|r"] = true
+
 L["Total:"] = true
+
+L["ERROR - Should be a hexadecimal code"] = true
+
 L["Total time played"] = true
+
 L[" days, "] = true
 L[" hours, "] = true
 L[" minutes, "] = true
