e8d72740a59d5d3b44f325aa95b89dd2b652e10a
LAMBOLEY Lucas <lucaslamboley@outlook.com>
2019-05-02 20:29:19 +0200

    1.00

--- locale/frFR.lua
+++ locale/frFR.lua
@@ -1,21 +1,43 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("AllTimePlayed", "frFR")
 if not L then return end
 
+L["Clean current character"] = "Nettoyer le personnage courant"
+L["Erase current character data"] = "Efface les données du personnage courant"
+L["Clean others characters"] = "Nettoyer les autres personnages"
+L["Erase others characters data"] = "Efface les données des autres personnages"
+
 L["Colors"] = "Couleurs"
+
+L["Color in minimap button"] = "Couleur dans le bouton de la minimap"
+L["Color in chat"] = "Couleur dans le chat"
+
 L["Current character"] = "Personnage actuel"
 L["Color of current character"] = "Couleur du personnage actuel"
 L["Others characters"] = "Autres personnages"
 L["Color of others characters"] = "Couleur des autres personnages"
 L["Total"] = "Total"
 L["Color of total"] = "Couleur du total"
-L["ERROR - Should be a hexadecimal code"] = "ERREUR - Doit être un code hexadécimale"
-L["Description"] = "Description"
-L["What does AllTimePlayed ?"] = "Que fais AllTimePlayed ?"
-L["It show the played time for all characters when pointer is on the minimap button."] = "Il affiche le temps total joué pour tout les personnages quand la souris est sur le bouton de la minimap."
-L["Clean data"] = "Nettoyer les données"
-L["Erase all data saved"] = "Supprime toutes les données sauvegardées"
+
+L["FAQ"] = "FAQ"
+L["Frequently Asked Questions"] = "Foire aux questions"
+L["What is AllTimePlayed ?"] = "Qu'est-ce que AllTimePlayed ?"
+L["It's an addon which record the played time per character. It give multiple way to print the info to the user."] = "C'est un addon qui enregistre le temps joué par personnage. Il offre plusieurs façon au joueur d'afficher l'information."
+L["When data are updated ?"] = "Quand est-ce que les données sont mise à jours ?"
+L["Data are updated when"] = "Les données sont mise à jours quand"
+L["You log in."] = "Vous vous connecté."
+L["You log out."] = "Vous vous déconnecté."
+L["You change of zone."] = "Vous changez de zone."
+L["You reload your UI."] = "Vous rechargez votre interface."
+L["You execute the /played command."] = "Vous executez la commande /played."
+L["I have found a bug, how can I contact you ?"] = "J'ai trouvé un bogue, comment puis-je te contacter ?"
+L["You can create an issue on my github |cffffff78<https://github.com/lamboley/AllTimePlayed>|r"] = "Vous pouvez créer une issue sur mon github |cffffff78<https://github.com/lamboley/AllTimePlayed>|r"
+
 L["Total:"] = "Total:"
+
+L["ERROR - Should be a hexadecimal code"] = "ERREUR - Doit être un code hexadécimale"
+
 L["Total time played"] = "Temps de jeu total"
+
 L[" days, "] = " jours, "
 L[" hours, "] = " heures, "
 L[" minutes, "] = " minutes, "
